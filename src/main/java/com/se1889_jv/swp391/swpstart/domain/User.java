7a8d357c92e6d1659490982918f580aebf645703
Vu Duy Le <vuduyle004@gmail.com>
2025-01-13 16:58:32 +0700

    Base project

--- src/main/java/com/se1889_jv/swp391/swpstart/domain/User.java
+++ src/main/java/com/se1889_jv/swp391/swpstart/domain/User.java
@@ -15,7 +15,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String username;
+    private String email;
     private String password;
     private String name;
     private String phone;
---
+++ src/main/java/com/se1889_jv/swp391/swpstart/service/UserService.java
@@ -0,0 +1,32 @@
+package com.se1889_jv.swp391.swpstart.service;
+
+import com.se1889_jv.swp391.swpstart.domain.User;
+import com.se1889_jv.swp391.swpstart.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByEmail(String email) {
+        Optional<User> user = this.userRepository.findByEmail(email);
+        if (user.isPresent()){
+            return user.get();
+        }
+        return null;
+    }
+
+    public User findById(long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        if (user.isPresent()){
+            return user.get();
+        }
+        return null;
+    }
+    
+}
