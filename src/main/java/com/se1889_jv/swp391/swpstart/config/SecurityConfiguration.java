70a34c87d533144e24522ed4c72c048768c0488b
Vu Duy Le <vuduyle004@gmail.com>
2025-01-14 17:21:11 +0700

    Authorize

--- src/main/java/com/se1889_jv/swp391/swpstart/config/SecurityConfiguration.java
+++ src/main/java/com/se1889_jv/swp391/swpstart/config/SecurityConfiguration.java
@@ -1,23 +1,20 @@
 package com.se1889_jv.swp391.swpstart.config;
 
 //import com.se1889_jv.swp391.swpstart.service.CustomUserDetailsService;
+
 import com.se1889_jv.swp391.swpstart.service.CustomUserDetailsService;
 import com.se1889_jv.swp391.swpstart.service.UserService;
 import jakarta.servlet.DispatcherType;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -64,10 +61,10 @@ public class SecurityConfiguration {
 //        return rememberMeServices;
 //    }
 //
-//    @Bean
-//    public AuthenticationSuccessHandler customSuccessHandler(UserService userService) {
-//        return new CustomSuccessHandler(userService);
-//    }
+    @Bean
+    public AuthenticationSuccessHandler customSuccessHandler(UserService userService) {
+        return new CustomSuccessHandler(userService);
+    }
 //
 //
 //    //Don't touch please
@@ -96,31 +93,35 @@ public class SecurityConfiguration {
 //
 //                .logout(logout -> logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
 //
-////                .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
-////
-////                .formLogin(formLogin -> formLogin
-////                        .loginPage("/login")
-////                        .failureUrl("/login?error")
-////                        .successHandler(customSuccessHandler(userService1))
-////                        .permitAll())
+//                .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
+//
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .failureUrl("/login?error")
+//                        .successHandler(customSuccessHandler(userService1))
+//                        .permitAll())
 //                .exceptionHandling(ex -> ex.accessDeniedPage("/access-deny"));
 //
 //        return http.build();
 //    }
+
+//Don't touch please
 @Bean
 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    UserService userService1 = null;
     http
             .authorizeHttpRequests(authorize -> authorize
                     .dispatcherTypeMatchers(DispatcherType.FORWARD,
                             DispatcherType.INCLUDE).permitAll()
 
-                    .requestMatchers("/","/login","/register", "/client/**", "/admin/**").permitAll()
+                    .requestMatchers("/", "/login", "/register", "/client/**", "/admin/**").permitAll()
                     .anyRequest().authenticated())
 
 
             .formLogin(formLogin -> formLogin
                     .loginPage("/login")
                     .failureUrl("/login?error")
+                    .successHandler(customSuccessHandler(userService1))
                     .permitAll());
 
     return http.build();
