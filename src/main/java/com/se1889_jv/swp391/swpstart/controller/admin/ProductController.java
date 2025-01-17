a3123b1e09a62f1711211715c9fa948b7cbc229a
duckthanh <thanhvttb2004@gmail.com>
2025-01-17 08:48:53 +0700

    table product

--- src/main/java/com/se1889_jv/swp391/swpstart/controller/admin/ProductController.java
+++ src/main/java/com/se1889_jv/swp391/swpstart/controller/admin/ProductController.java
@@ -1,4 +1,16 @@
 package com.se1889_jv.swp391.swpstart.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
+
+    @GetMapping("/product")
+    public String getListProductPage(Model model) {
+
+        return "admin/product/table";
+    }
+
 }
